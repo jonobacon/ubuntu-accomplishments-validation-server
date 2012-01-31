0f0689c7a166993f5a7ef286d15789d1858ac688
Jono Bacon <jono@ubuntu.com>
2012-01-30 16:10:44 -0800

    First cut. Added brit.py which sets up the queue.

---
+++ brit.py
@@ -0,0 +1,62 @@
+import os
+import ConfigParser
+from optparse import OptionParser
+
+SHARESDIR = None
+QUEUEDIR = None
+
+all = []
+signed = []
+matches = []
+final = []
+
+class Brit(object):
+    def __init__(self, config_path):
+        config = ConfigParser.ConfigParser()
+        config.read(config_path + ".matrix")
+
+        SHARESDIR = config.get("matrix", "sharesdir")
+        QUEUEDIR = config.get("matrix", "queuedir")
+
+        for r,d,f in os.walk(SHARESDIR):
+            for i in f:
+                if i.endswith(".trophy.sig"):
+                    res = os.path.join(r,i)
+                    signed.append(res[:-4])
+                all.append(os.path.join(r,i))
+
+        for i in all:
+            if i.endswith(".sig"):
+                pass
+            else:
+                matches.append(i)
+
+        final = list(set(matches) - set(signed))
+
+        # clear out links from queue
+
+        for the_file in os.listdir(QUEUEDIR + "/"):
+            file_path = os.path.join(QUEUEDIR + "/", the_file)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+            except Exception, e:
+                print e
+
+        # create the symbol links
+
+        i = 1
+
+        # populating the queue
+
+        for t in final:
+            print "Adding: " + t
+            os.symlink(t, QUEUEDIR + "/" + str(i))
+            i = i + 1
+
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.set_defaults(suppress_notifications=False)
+    parser.add_option("--config-path", dest="config_path", default=os.getenv("HOME") + "/")
+    options, args = parser.parse_args()
+    foo = Brit(config_path=options.config_path)
