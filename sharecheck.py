297762b49bdb3c6e4011e0a5b4c6961d3c107f9c
Jono Bacon <jono@ubuntu.com>
2012-03-02 21:11:39 +0000

    Added process-sharecheck to ensure all shares are accepted and subscribed to.

--- sharecheck.py
+++ sharecheck.py
@@ -18,17 +18,20 @@ class ShareAccept():
 
 	@defer.inlineCallbacks
 	def detect_shares(self):
+		print "Detecting shares..."
 		self.sd.refresh_shares()
 		l = yield self.sd.get_shares()
 		for s in l:
+			volume_id = str(s["volume_id"])
 			if s["accepted"] == "":
 				print "...FOUND NEW SHARE: " + str(s["name"]) + " (" + str(s["volume_id"]) + ")"
-				volume_id = str(s["volume_id"])
 				print "...accepting share: " + volume_id
 				res = yield self.sd.accept_share(volume_id)
+			if s["subscribed"] == "":
 				print "...subscribing to share: " + volume_id
 				sub = yield self.sd.subscribe_share(volume_id)
 				print "...done!"
+		print "Completed."
 		reactor.stop()		
 
 if __name__ == '__main__':
