93cb811ee7c72d0cc33364ca4fdae3c1d32698b4
Jono Bacon <jono@ubuntu.com>
2012-08-02 01:02:39 +0100

    * Updated the example .matrixexample file.  * Adjusted a hard coded file path in process-sharecheck.  * Removed some files.  * Updated the README with deployment instructions.

--- cron_sh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if ps -ef | grep -v grep | grep sharecheck.py ; then
-	exit 0
-else
-	~/validation-service/process-sharecheck &
-fi
--- shares.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-while true; do
-  python shares.py
-done
